3c4c635aff854b3f71388ebd4b68a1f8c34e3ee4
gerry-baird <gerry.baird@uk.ibm.com>
2025-05-07 14:45:02 +0000

    Two agents

--- tools/life_interest_rates.py
+++ tools/life_interest_rates.py
@@ -2,9 +2,9 @@ import random
 from ibm_watsonx_orchestrate.agent_builder.tools import tool, ToolPermission
 
 @tool(name="lifeInterestRateLookup",
-      description="This tool will determine the interest rate to be used when calculating monthly payments for life policies. The rate will be based on the value of the policy.",
+      description="This tool will determine the interest rate to be used when calculating monthly payments for life policies. The rate will be based on the annual premium of the policy.",
       permission=ToolPermission.ADMIN)
-def rate_lookup(value:float):
+def rate_lookup(premium:float):
     rate = 15
 
     rate = random.randint(50, 100)
